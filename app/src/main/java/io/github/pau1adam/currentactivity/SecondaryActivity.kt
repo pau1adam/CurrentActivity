ce3b1b674637fad78f10cbb2b42c9b0d7839a06e
Pavlos Adam <pavlosadam@gmail.com>
2018-09-14 22:29:05 +0300

    Added secondary activity to sample.

---
+++ app/src/main/java/io/github/pau1adam/currentactivity/SecondaryActivity.kt
@@ -0,0 +1,12 @@
+package io.github.pau1adam.currentactivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+    }
+}
